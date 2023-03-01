83d31c73985b14f0d99053850e1467a742723c51
Henderson Vicente Rodrigues <92233052+Seuvicente@users.noreply.github.com>
2023-03-01 15:09:02 -0300

    Add files via upload

---
+++ exercicio55.js
@@ -0,0 +1,44 @@
+// 55. Na teoria dos sistemas, define-se como elemento minimax de uma matriz o menor elemento da linha onde se
+// encontra o maior elemento da matriz. Escreva um algoritmo que leia uma matriz 10 X 10 de números e encontre seu
+// elemento minimax, mostrando também sua posição.
+
+//Gero os numeros aleatórios e insere na matriz
+const matriz = []
+
+for(lin=0;lin<10;lin++){
+    matriz[lin] = []
+    for(col=0;col<10;col++){
+        let aleatorio = Math.random()*100
+        let num = aleatorio.toFixed(0) - 1
+        matriz[lin][col] = num
+    }
+}
+
+//Encontra o maior valor dentro da matriz e a posição
+let maior = matriz[0][0]
+let posL  
+let posC  
+
+for(l=0;l<matriz.length;l++){
+    for(c=0;c<matriz.length;c++){
+        if(matriz[l][c] > maior){
+            maior = matriz[l][c]
+            posL = l
+            posC = c
+        }
+    }
+}
+
+//Encontrando o minimax
+let menor = matriz[posL][0]
+
+for(m=0;m<=10;m++){
+    if(matriz[posL][m] < menor ){
+        console.log(menor,matriz[posL][m])
+        menor = matriz[posL][m]
+    }
+}
+
+//exibindo resultado
+console.table(matriz)
+console.log("Maior numero: ",maior,",Linha :",posL,", Minimax: ", menor)
